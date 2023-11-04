fed26881be5cc91770f38f61e43e7475003ecb98
suchT01 <zorrillaja30@gmail.com>
2023-11-03 20:23:07 -0400

    name carpet changed

--- view/home.php
+++ view/home.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="../src/styles.css">
-    <link rel="stylesheet" href="../src/fontawesome/css/all.css">
+    <link rel="stylesheet" href="../public/css/styles.css">
+    <link rel="stylesheet" href="../public/fontawesome/css/all.css">
     <title>MAKING FOOD</title>
 </head>
 <body>
